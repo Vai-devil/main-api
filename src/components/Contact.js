9f7cc7b0e6bb72367aa80d5bce8e04fb3fcd185d
Vaibhav Heda <Vaibhav.16BCON414@jecrcu.edu.in>
2019-11-03 22:29:01 +0530

    Added Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,40 @@
 import React from "react";
+import "../css/Contact.css";
 
 const Contact = () => {
 	return (
-		<div>
-			<h1 className="profileName">Conact</h1>
+		<div className="contactPage">
+			<div className="bottomCorner"></div>
+			<div className="contactHeading">
+				Contact Me
+				<br />
+			</div>
+			<div className="contacts">
+				<div className="email">
+					<b>E-Mail : </b>
+					<a href="mailto:Vaibhav.heda799@gmail.com">Mail Me!</a>
+				</div>
+				<br />
+				<div>
+					<b>Instagram : </b>
+					<a href="https://www.instagram.com/vai_heda/">@Vai_heda</a>
+				</div>
+				<br />
+				<div>
+					<b>Facebook : </b>
+					<a href="https://www.facebook.com/vaibhavheda.36">Vaibhav Heda</a>
+				</div>
+				<br />
+				<div>
+					<b>Linked in : </b>
+					<a href="https://www.linkedin.com/in/vaibhav-h-a5383b109/">Vaibhav</a>
+				</div>
+				<br />
+				<div>
+					<b>Github: </b>
+					<a href="https://github.com/Vai-devil">Vai-devil</a>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/canvas.js
+++ src/components/canvas.js
@@ -6,10 +6,10 @@ const sample1 = {
 	colors: ["#2E1D62", "#513D91", "#487EEF", "#11A887", "#fc5c65", "#fed330"],
 	shapes: ["circle", "square", "Rectangle"],
 	size: 15,
-	minSpeed: 0.1,
+	minSpeed: 0.2,
 	maxSpeed: 0.8,
 	alpha: 0.7,
-	backgroundColor: "#1E1F29"
+	backgroundColor: "#202020"
 };
 
 export default class Can extends Component {
